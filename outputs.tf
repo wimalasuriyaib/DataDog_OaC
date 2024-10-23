19cae5bc354a753e4b51d397441040a8605d2b2a
Lachitha Senarath Yapa <87459623+Lachitha@users.noreply.github.com>
2024-10-23 19:58:07 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "google_api_monitor_id" {
-  value = datadog_synthetics_test.google_api_monitor.id
-}
-
-output "google_browser_monitor_id" {
-  value = datadog_synthetics_test.google_browser_monitor.id
-}
