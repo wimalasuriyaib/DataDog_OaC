8e7489b0fd73b1e61f6142eb265301437584504f
Lachitha Senarath Yapa <87459623+Lachitha@users.noreply.github.com>
2024-10-23 20:04:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,30 +12,7 @@ provider "datadog" {
   app_key = var.datadog_app_key
 }
 
-# API Monitor for google.com
-resource "datadog_synthetics_test" "google_api_monitor" {
-  type   = "api"
-  name   = "Google API Monitor"
-  status = "live"
 
-  request {
-    method  = "GET"
-    url     = "https://www.google.com"
-    headers = {
-      "Content-Type" = "application/json"
-    }
-  }
-
-  locations = ["aws:us-east-1"]
-
-  options {
-    tick_every           = 300   # Check every 5 minutes
-    min_failure_duration = 60    # Minimum duration (in seconds) before failing the test
-    min_location_failed  = 1      # At least one location must fail for the test to be considered a failure
-  }
-
-  tags = ["environment:production"] # Optional: Add tags for better organization
-}
 
 # Define variables for API and App keys
 variable "datadog_api_key" {
