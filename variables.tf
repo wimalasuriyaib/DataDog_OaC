3efd3576362af4867e80f4020374f1582e581026
Lachitha Senarath Yapa <87459623+Lachitha@users.noreply.github.com>
2024-10-23 19:26:42 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "datadog_api_key" {
+  description = "Datadog API Key"
+  type        = string
+}
+
+variable "datadog_app_key" {
+  description = "Datadog Application Key"
+  type        = string
+}
