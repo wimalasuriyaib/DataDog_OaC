76d31f7db396610206e7700f6094a9da3e20949a
Lachitha Senarath Yapa <87459623+Lachitha@users.noreply.github.com>
2024-10-23 19:58:18 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "datadog_api_key" {
-  description = "Datadog API Key"
-  type        = string
-}
-
-variable "datadog_app_key" {
-  description = "Datadog Application Key"
-  type        = string
-}
